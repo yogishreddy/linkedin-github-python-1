8f3933bbf3b36dd4bea03c088d29dc7506dd1d33
yogishreddy <114511059+yogishreddy@users.noreply.github.com>
2022-11-13 08:44:16 +0000

    added dataset

--- Ex_Files/03_01_begin/main.py
+++ Ex_Files/03_01_begin/main.py
@@ -1,17 +1,16 @@
 import csv
 from pprint import pprint
 
-EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
+# EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
 
-EINSTEIN = {
-    "birthplace": "Germany",
-    "name": "Albert",
-    "surname": "Einstein",
-    "born": "1879-03-14",
-    "category": "physics",
-    "motivation": "for his services to Theoretical Physics...",
-}
-
+# EINSTEIN = {
+#     "birthplace": "Germany",
+#     "name": "Albert",
+#     "surname": "Einstein",
+#     "born": "1879-03-14",
+#     "category": "physics",
+#     "motivation": "for his services to Theoretical Physics...",
+# }
 with open("laureates.csv", "r") as f:
     reader = csv.DictReader(f)
     laureates = list(reader)
