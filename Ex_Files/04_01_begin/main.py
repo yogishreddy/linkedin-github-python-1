dde2ef40553260f5700afa89ac95672f6cecf52f
yogishreddy <114511059+yogishreddy@users.noreply.github.com>
2022-11-14 14:03:39 +0000

    changes

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -1,6 +1,8 @@
 import requests
+from pprint import pprint
 
 response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
 
-last_twenty_years = response.json()[1][:20]
+last_twenty_years = response.json()[1][:2]
+pprint(last_twenty_years)
